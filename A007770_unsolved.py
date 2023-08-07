124f15427e81515bb6d1aec4f37614414543481e
Bryan Tremblay <bryanandhallie@gmail.com>
2023-08-07 14:01:31 -0400

    Keeping the Happy Numbers solution unsolved for now

---
+++ A007770_unsolved.py
@@ -0,0 +1,19 @@
+# [NOTE] This entry is unsolved due to me not understanding how to formally generate the repeating patterns
+# These are listed under https://oeis.org/A039943 and although I can manually find them I have
+# no idea if I've found them all. I understand there are very few through OEIS but I would rather understand
+# and then solve this more formally. Until then I'm going to keep this one open but unsolved
+
+from utils.Solution import Solution
+from utils.Math import *
+
+N = 0
+def logic() -> list:
+    digits = []
+
+    # Logic here
+
+    return digits    # May omit for brevity
+
+solution = Solution(N, "Happy Numbers")
+solution.logic = logic
+solution.run()
